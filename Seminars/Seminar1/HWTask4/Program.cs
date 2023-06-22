abe8645e9d46dc31fd50086c21504ce834d6d7ea
TatianaNikitina <nikitina.tania.msu@gmail.com>
2023-06-23 00:18:39 +0300

    HW task3

---
+++ Seminars/Seminar1/HWTask4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+int num;
+do{
+    Console.Write("Enter a non-negative number: ");
+    num = int.Parse(Console.ReadLine());
+}
+while (num < 0);
+
+if (num < 2) {Console.WriteLine("There are no valid numbers");}
+
+for (int i = 2; i <= num; i=i+2)
+{
+    Console.Write($"{i} ");
+}
+Console.WriteLine();
