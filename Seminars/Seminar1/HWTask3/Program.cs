b659422d81a55be2d42c919341a5599c78511aae
TatianaNikitina <nikitina.tania.msu@gmail.com>
2023-06-22 23:57:19 +0300

    HW task3

---
+++ Seminars/Seminar1/HWTask3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0){
+    Console.WriteLine($"{number} is even");
+}else{
+    Console.WriteLine($"{number} is odd");
+}
