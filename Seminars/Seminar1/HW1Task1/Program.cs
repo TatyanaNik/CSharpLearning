3f786e391c6a6d2a12bf37977dee6c5a8306297b
TatianaNikitina <nikitina.tania.msu@gmail.com>
2023-06-22 23:27:32 +0300

    First HW task

---
+++ Seminars/Seminar1/HW1Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+Console.Write("Please, enter first integer number: ");
+int firstNum = int.Parse(Console.ReadLine());
+Console.Write("Please, enter second integer number: ");
+int secondNum = int.Parse(Console.ReadLine());
+
+if (firstNum > secondNum){
+   Console.WriteLine($"{firstNum} bigger then {secondNum}"); 
+}
+else if (firstNum < secondNum){
+   Console.WriteLine($"{firstNum} smaller then {secondNum}"); 
+}
+else{
+     Console.WriteLine("Numbers are equal");
+}
