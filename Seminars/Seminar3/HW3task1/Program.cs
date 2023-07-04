1e974b9cdf96ac04b5d2bb7e72d6ba6a2d0a1cc8
Nikitina Tatiana <nikitinat@pc-1.k113>
2023-07-04 23:02:25 +0300

    Seminar3 finish commit

--- Seminars/Seminar3/HW3task1/Program.cs
+++ Seminars/Seminar3/HW3task1/Program.cs
@@ -1,17 +1,10 @@
 ﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
 //Methods///////////////////////////////////////////////////////////////
+
 /*string GetData (){
     Console.Write("insert value:");
     string NumStr = Console.ReadLine();
-    if (int.TryParse(NumStr, out var x))
-    {
-        return NumStr;
-    }
-    else
-    {
-        Console.WriteLine("Некорректный ввод");
-        return "null";
-    }
+
     if (Num < 100000 && Num > 9999)
     {
         return Num.ToString();
@@ -22,6 +15,7 @@
         return "NULL";
     }
 }*/
+
 void CheckPal(string str){
     bool checkFlag = true;
     for (int i =0; i < str.Length/2; i++){
@@ -39,7 +33,7 @@ void CheckPal(string str){
 }
 ////////////////////////////////////////////////////////////////////////
 
-    Console.Write("insert value: ");
+    Console.Write("Введите число: ");
     string NumStr = Console.ReadLine();
     if (int.TryParse(NumStr, out var x))
     {
