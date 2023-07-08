a78016d0eb311af89f27abf4121060b7baf415a8
TatianaNikitina <nikitina.tania.msu@gmail.com>
2023-07-08 15:57:30 +0300

    Add Seminar5 HW

---
+++ Seminars/Seminar5/HWtask3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементами массива
+
+//Methods////////////////////////
+double[] DefRealArray(int size)
+{
+    double[] array = new double[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = Convert.ToDouble(new Random().Next(-100, 101)) / Convert.ToDouble(new Random().Next(1, 11));
+
+    var str = string.Join(";  ", array);
+    Console.WriteLine(str);
+
+    return array;
+
+}
+/////////////////////////////////
+
+double[] testArray = DefRealArray(5);
+
+//double result = testArray.Max() - testArray.Min();
+
+Console.WriteLine("Max - Min = " + (testArray.Max() - testArray.Min()));
